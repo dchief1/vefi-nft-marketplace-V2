14ca967ec5de73ad264987a6abaee05bf06cced6
Kingsley Victor <javaprodigy56@gmail.com>
2022-09-08 18:45:58 +0100

    set tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./components/**/*.{ts,tsx}', './pages/**/*.{ts,tsx}', './styles/**/*.{css}'],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+};
