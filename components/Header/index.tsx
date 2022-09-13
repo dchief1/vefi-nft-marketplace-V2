c21659fe9078b06b7d7b446a7c0c73d6542cf266
Emmanuel Igwesi <igwesiemmanuel4@gmail.com>
2022-09-13 03:08:24 -0700

    created a default footer and header

---
+++ components/Header/index.tsx
@@ -0,0 +1,35 @@
+import Link from 'next/link'; 
+import { FaSearch } from 'react-icons/fa';
+
+
+export const index = () => {
+  return (
+    <div className='bg-blue-800'>
+      <div className='flex justify-between items-center p-6'>
+
+        <img 
+            className=''
+            src='/Logo.jpeg'
+            alt='logo' 
+        />
+
+      <div className='flex justify-between items-center gap-4'>
+        <div className='text-black bg-[#FFE600] rounded-lg '>
+          <Link href='/'>
+            <span className='grid place-items-center mx-6 my-2 text-md'>Connect wallet</span>
+          </Link>
+        </div>
+
+        <span className='text-red-200 text-2xl'>
+          <FaSearch />
+        </span>
+        </div>
+        
+      </div>
+
+    </div>
+  )
+}
+
+export default index;
+
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,14 @@
+import Head from 'next/head'
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className='min-h-100vh'>
+      <Head>
+        <title>Vefi NFT Marketplace</title>
+        <meta name="description" content="vefi nft marketplace" />
+        <link rel="icon" href="" />
+      </Head>
       <span>Homepage goes here! Like seriously, it does</span>
     </div>
   );
