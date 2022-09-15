0f3b40a018ad57be71d0376a3db4ace4e32c9b25
Emmanuel Igwesi <igwesiemmanuel4@gmail.com>
2022-09-15 12:48:04 -0700

    formatted code with prettier

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,15 +4,17 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <div className='bg-[#03015A]'> 
-    <Header />
+  return (
+    <div className="bg-[#03015A]">
+      <Header />
 
-    <div className='min-h-screen'>
-      <Component {...pageProps} />;
+      <div className="min-h-screen">
+        <Component {...pageProps} />;
+      </div>
+
+      <Footer />
     </div>
-
-    <Footer />
- </div>
+  );
 }
 
 export default MyApp;
