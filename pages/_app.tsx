509c718cbf842f4c2ee6bf0eb8adc77b2aaa7998
Kingsley Victor <javaprodigy56@gmail.com>
2022-10-14 21:00:42 +0100

    ...

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,9 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import { useRouter } from 'next/router';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter();
   return (
     <div
       className={`flex justify-center  flex-col min-h-screen w-screen gap-4`}
